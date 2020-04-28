6fbd0eaf13c7a3b98bd50fbcd3b289f6698725f8
hrmq <h.rahimi.moqadam@gmail.com>
2020-04-28 19:56:29 -0400

    add cache function to mongoose query exec

--- services/cache.js
+++ services/cache.js
@@ -1,8 +1,37 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const redisUrl = 'redis://localhost:6379';
+const client = redis.createClient(redisUrl);
+const util = require('util');
 
+client.get = util.promisify(client.get);
+client.set = util.promisify(client.set);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
+mongoose.Query.prototype.exec = async function () {
   console.log("I'M ABOUT  TO RUN A QUERY");
-  return exec.apply(this, arguments);
+
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
+  console.log(key);
+  // see if we have a value for key
+  const cacheValue = await client.get(key);
+
+  // if we do return that
+  if (cacheValue) {
+    console.log('READ FROM CACHE');
+    const doc = JSON.parse(cacheValue);
+
+    return Array.isArray(doc)
+      ? doc.map((d) => new this.model(d))
+      : new this.model(doc);
+  }
+
+  // Otherwise, issue the query and store the result in redis
+  const result = await exec.apply(this, arguments);
+  client.set(key, JSON.stringify(result));
+  return result;
 };
