8061705c1bcd76822562de021885456b412d4a3f
hrmq <h.rahimi.moqadam@gmail.com>
2020-05-11 14:09:33 -0400

    add test for checking web site title

--- client/tests/header.test.js
+++ client/tests/header.test.js
@@ -5,4 +5,8 @@ test('We can launch a browser', async () => {
     headless: false,
   });
   const page = await browser.newPage();
+  await page.goto('http://localhost:3000');
+
+  const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+  expect(text).toEqual('Blogster');
 });
