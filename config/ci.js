ee51beb8b4f9e1652eb361f25bbdefeac2b194fa
hrmq <h.rahimi.moqadam@gmail.com>
2020-06-01 14:50:21 -0400

    add ci related configuration

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '1096269823748-tmb0j0n9jdosjs6gekpvh3obvfb9uspb.apps.googleusercontent.com',
   googleClientSecret: 'YMZb-KlN0OsAWzT5AwmssK8P',
-  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  mongoURI: 'mongodb://localhost:27017/blog_everyone',
   redisUrl: 'redis://127.0.0.1:6379',
   cookieKey: '123123123',
 };
--- services/cache.js
+++ services/cache.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
-const redisUrl = 'redis://localhost:6379';
-const client = redis.createClient(redisUrl);
 const util = require('util');
+const keys = require('../config/keys');
 
+const client = redis.createClient(keys.redisUrl);
 client.hget = util.promisify(client.hget);
+
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.cache = function (options = {}) {
